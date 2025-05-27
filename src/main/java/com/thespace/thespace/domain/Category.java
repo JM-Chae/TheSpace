3f31a6673201548d8c55ca8fd67a9edc914fbfe2
JM-Chae <jm.chae1998@gmail.com>
2025-05-27 21:35:18 +0900

    Updated CategoryName field not unique.

--- src/main/java/com/thespace/thespace/domain/Category.java
+++ src/main/java/com/thespace/thespace/domain/Category.java
@@ -1,7 +1,6 @@
 package com.thespace.thespace.domain;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -26,7 +25,6 @@ public class Category extends BaseEntity {
     @NotNull
     private String path;
 
-    @Column(unique = true)
     @NotNull
     private String categoryName;
 
